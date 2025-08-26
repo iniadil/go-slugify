12a9a5e05b21e986079a2aec3bae8658e5787656
Adil Muhammad <ivgcomunity@gmail.com>
2025-08-26 15:59:45 +0700

    minify test

--- test.go
+++ test.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	//slug, err := go_slugify.Slugify("Slugify - make your title better")
-	slug, _ := slugify.Slugify("Slugify - make !@#$%^&*()")
-
-	fmt.Println(slug)
+	//slug, _ := slugify.Slugify("Slugify - make !@#$%^&*()")
+	//
+	//fmt.Println(slug)
 	//if err == nil {
 	//}
 
-	newSlug, _ := slugify.Slugify("Slugify - make !@#$%^&*()", slugify.WithSeparator("/"))
+	newSlug, _ := slugify.Slugify("Slugify - make !@#$%^&*()", slugify.WithLowerCase(false))
 	fmt.Println(newSlug)
 }
