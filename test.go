cabf00bec05ca38bc69f7a6bec8f69b8e9189729
Adil Muhammad <ivgcomunity@gmail.com>
2025-08-26 14:34:29 +0700

    test the func

--- test.go
+++ test.go
@@ -1,1 +1,16 @@
-package go_slugify
+package main
+
+import (
+	"fmt"
+
+	go_slugify "github.com/iniadil/go-slugify/helper"
+)
+
+func main() {
+	//slug, err := go_slugify.Slugify("Slugify - make your title better")
+	slug, err := go_slugify.Slugify("Slugify - make !@#$%^&*()")
+
+	if err == nil {
+		fmt.Println(slug)
+	}
+}
