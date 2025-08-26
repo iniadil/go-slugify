31aab60909d192af34ba69aea2b7351ade404474
Adil Muhammad <ivgcomunity@gmail.com>
2025-08-26 15:59:29 +0700

    add benchmark test

--- test/slugify_test.go
+++ test/slugify_test.go
@@ -1,15 +1,28 @@
 package test
 
 import (
-	"strconv"
 	"testing"
 
 	"github.com/iniadil/go-slugify/slugify"
 )
 
-func BenchmarkSlugify(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		title := "test" + strconv.Itoa(i)
-		slug, _ := slugify.Slugify(title)
+func BenchmarkSlugify_Table(b *testing.B) {
+	cases := []struct {
+		name string
+		in   string
+		opts []slugify.Options // atau []func(*config) tergantung penamaanmu
+	}{
+		{"short_default", "Halo Dunia", nil},
+		{"long_default", "…panjang sekali …", nil},
+		{"custom_sep", "Halo___Dunia", []slugify.Options{slugify.WithSeparator("_")}},
+		{"no_lower", "Halo Dunia", []slugify.Options{slugify.WithLowerCase(false)}},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _ = slugify.Slugify(tc.in, tc.opts...)
+			}
+		})
 	}
 }
